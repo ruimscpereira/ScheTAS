0facd1f84cf0d7e7a1e794f716f6cd5e720ffd23
ruimscpereira <ruimscpereira@gmail.com>
2026-09-19 14:49:48 +0100

    Update app title to include TAS Imagiologia ULSRA

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ if "confirmar_substituicao" not in st.session_state:
 # =============================================================================
 # SELETOR GLOBAL DE MÊS E ANO NO TOPO
 # =============================================================================
-st.title("🗓️ Gestor Inteligente de Escalas de Trabalho")
+st.title("🗓️ ScheTAS - Gestor Inteligente de Escalas de Trabalho - TAS Imagiologia ULSRA")
 
 col_g1, col_g2, col_g3 = st.columns([2, 2, 4])
 with col_g1:
