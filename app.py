15f61d531f0bf7d53304effb19bde8ccee5ebe58
ruimscpereira <ruimscpereira@gmail.com>
2026-09-19 18:22:05 +0100

    Fix title text in app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ if "confirmar_substituicao" not in st.session_state:
 # =============================================================================
 # SELETOR GLOBAL DE MÊS E ANO NO TOPO
 # =============================================================================
-st.title("🗓️ ScheTAS - Gestor Inteligente de Escalas de Trabalho - TAS Imagiologia")
+st.title("🗓️ ScheTAS - Gestor de Escalas de Trabalho - TAS Imagiologia")
 
 col_g1, col_g2, col_g3 = st.columns([2, 2, 4])
 with col_g1:
